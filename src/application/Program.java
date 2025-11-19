2840185fea5170bfd307df0a482e2634f6c335bb
John Lima <johncleciofl@gmail.com>
2025-11-19 20:05:19 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,11 +25,15 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n==== TEST 1: seller findAll ====");
+        System.out.println("\n==== TEST 3: seller findAll ====");
         list = sellerDao.findAll();
         for (Seller obj : list ){
             System.out.println(obj);
         }
+        System.out.println("\n==== TEST 4: seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00,  department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
 
 
 
