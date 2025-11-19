0c77f2dbf3147a76bba2df213c331c17b617b548
John Lima <johncleciofl@gmail.com>
2025-11-19 20:35:19 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,6 +36,12 @@ public class Program {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
 
+        System.out.println("\n==== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        seller.setEmail("martha@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
 
     }
 }
